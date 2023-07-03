2c914bce29a1c8ebbaabe619807c88536fb267c3
Yoosuf Nixam <nixxaam21@hotmail.com>
2023-07-03 09:17:36 +0000

    Pending changes exported from your codespace

---
+++ RazorPagesPizza/Areas/Identity/Data/RazorPagesPizzaUser.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the RazorPagesPizzaUser class
+public class RazorPagesPizzaUser : IdentityUser
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+}
+
